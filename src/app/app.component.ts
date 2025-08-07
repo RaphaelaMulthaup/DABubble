0e1df0d918f65a6b79b5960f01f57550c2541e7f
Raphaela Multhaup <raphaelamulthaup@icloud.com>
2025-08-07 09:59:44 +0200

    feat: Firestore injected

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { AsyncPipe } from '@angular/common';
+import { Firestore } from '@angular/fire/firestore';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, AsyncPipe],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'DABubble';
+  firestore: Firestore = inject(Firestore);
+
+  constructor() {
+
+  }
 }
