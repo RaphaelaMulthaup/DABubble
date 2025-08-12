28a319a52691e1658a07dc3bd1e1eeb96eb19af0
Raphaela Multhaup <raphaelamulthaup@icloud.com>
2025-08-12 08:48:50 +0200

    feat: create chat.service, chat.interface and massage.interface

---
+++ src/app/services/chat.service.ts
@@ -0,0 +1,10 @@
+import { inject, Injectable } from '@angular/core';
+import { Firestore } from '@angular/fire/firestore';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ChatService {
+     private firestore: Firestore = inject(Firestore);
+
+}
---
+++ src/app/shared/models/massage.interface.ts
@@ -0,0 +1,6 @@
+export interface Message {
+  senderId: string;
+  text?: string;
+  createdAt: any;
+  fileUrl?: string[];
+}
