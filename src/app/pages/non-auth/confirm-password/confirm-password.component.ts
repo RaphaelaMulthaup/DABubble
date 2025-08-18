6434240563ff29a5e40a898b52e63271b0e7d322
NicolausFeldtmann <n.feldtmann@yahoo.de>
2025-08-18 18:00:57 +0200

    password confirm / reset added

--- src/app/pages/non-auth/confirm-password/confirm-password.component.ts
+++ src/app/pages/non-auth/confirm-password/confirm-password.component.ts
@@ -1,11 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { AuthService } from '../../../services/auth.service';
+import { FormControl, FormsModule, ReactiveFormsModule, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-confirm-password',
-  imports: [],
+  imports: [FormsModule, ReactiveFormsModule],
   templateUrl: './confirm-password.component.html',
   styleUrl: './confirm-password.component.scss'
 })
 export class ConfirmPasswordComponent {
+  confirmForm: FormGroup = new FormGroup({})
+
+  onSubmit() { }
 
 }
