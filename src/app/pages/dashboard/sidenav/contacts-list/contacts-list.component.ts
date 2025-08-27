c24236c7cbfec1c49465031e317e27c62a0d2821
Raphaela Multhaup <119539243+RaphaelaMulthaup@users.noreply.github.com>
2025-08-27 14:45:36 +0200

    feat: currentUser in contacts-list

--- src/app/pages/dashboard/sidenav/contacts-list/contacts-list.component.ts
+++ src/app/pages/dashboard/sidenav/contacts-list/contacts-list.component.ts
@@ -3,7 +3,7 @@ import { CommonModule, AsyncPipe } from '@angular/common';
 import { UserService } from '../../../../services/user.service';
 import { AuthService } from '../../../../services/auth.service';
 import { ChatService } from '../../../../services/chat.service';
-import { Observable, of, switchMap, map, combineLatest } from 'rxjs';
+import { Observable, of, switchMap, map, combineLatest, filter } from 'rxjs';
 import { UserInterface } from '../../../../shared/models/user.interface';
 import { ContactListItemComponent } from './contact-list-item/contact-list-item.component';
 
@@ -16,10 +16,11 @@ import { ContactListItemComponent } from './contact-list-item/contact-list-item.
 export class ContactsListComponent implements OnInit {
   // Holds the list of contacts as an observable
   contacts$: Observable<UserInterface[]> = of([]);
-  // Stores the currently logged-in user's ID
-  currentUserId: string | null = null;
+  // // Stores the currently logged-in user's ID
+  // currentUserId: string | null = null;
   // Controls visibility of direct messages section
   directMessagesVisible = true;
+  currentUser!: UserInterface;
 
   constructor(
     private userService: UserService,
@@ -32,6 +33,10 @@ export class ContactsListComponent implements OnInit {
    * Subscribes to the authenticated user stream and loads the user's contacts.
    */
   ngOnInit(): void {
+    this.authService.currentUser$
+    .pipe(filter((user): user is UserInterface => user !== null))
+    .subscribe(user => this.currentUser = user);
+    
     this.contacts$ = this.authService.user$.pipe(
       switchMap((user) => {
         if (!user) {
