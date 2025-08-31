3e254afcfe8a11b12144e2fde68d81e9e84d4b00
Andrei <buha2595@gmail.com>
2025-08-31 21:25:16 +0200

    feat: i created a component to display all members from channel

--- src/app/overlay/edit-channel/channel-members/channel-members.component.ts
+++ src/app/overlay/edit-channel/channel-members/channel-members.component.ts
@@ -1,11 +1,22 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, inject } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Observable } from 'rxjs';
+import { UserInterface } from '../../../shared/models/user.interface';
+import { UserService } from '../../../services/user.service';
 
 @Component({
   selector: 'app-channel-members',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './channel-members.component.html',
   styleUrl: './channel-members.component.scss'
 })
 export class ChannelMembersComponent {
 @Input() memberIds?:string[];
+
+private userService = inject(UserService);
+users$?:Observable<UserInterface[]>;
+
+ngOnInit(){
+  this.users$ = this.userService.getMembersFromChannel(this.memberIds!);
+}
 }
