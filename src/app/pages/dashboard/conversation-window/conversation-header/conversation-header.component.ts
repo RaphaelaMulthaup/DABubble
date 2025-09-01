a6569139bdc0c50cfea80f173bbdc611b3660045
Raphaela Multhaup <119539243+RaphaelaMulthaup@users.noreply.github.com>
2025-09-01 16:47:32 +0200

    feat: header-searchbar in conversation-header

--- src/app/pages/dashboard/conversation-window/conversation-header/conversation-header.component.ts
+++ src/app/pages/dashboard/conversation-window/conversation-header/conversation-header.component.ts
@@ -4,11 +4,12 @@ import { ChatActiveRouterService } from '../../../../services/chat-active-router
 import { ActivatedRoute, Router } from '@angular/router';
 import { UserService } from '../../../../services/user.service';
 import { HeaderChannelComponent } from "./header-channel/header-channel.component";
+import { HeaderSearchbarComponent } from "./header-searchbar/header-searchbar.component";
 
 
 @Component({
   selector: 'app-conversation-header',
-  imports: [CommonModule, HeaderChannelComponent],
+  imports: [CommonModule, HeaderChannelComponent, HeaderSearchbarComponent],
   templateUrl: './conversation-header.component.html',
   styleUrl: './conversation-header.component.scss',
 })
