8307a31916a99da2f9be77933720f9254c34928c
Andrei <buha2595@gmail.com>
2025-09-05 22:21:09 +0200

    feat: i added overlay user list-itom-to-channel/ than 2 signals in overlayservice. [users/searchReset]

--- src/app/overlay/add-member-to-channel/add-member-to-channel.component.ts
+++ src/app/overlay/add-member-to-channel/add-member-to-channel.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, inject } from '@angular/core';
+import { Component, Input, effect, inject } from '@angular/core';
 import { HeaderOverlayComponent } from '../../shared/components/header-overlay/header-overlay.component';
 import { ChannelInterface } from '../../shared/models/channel.interface';
 import { CommonModule } from '@angular/common';
@@ -11,6 +11,8 @@ import { UserInterface } from '../../shared/models/user.interface';
 import { ChannelsService } from '../../services/channels.service';
 import { Overlay } from '@angular/cdk/overlay';
 import { OverlayService } from '../../services/overlay.service';
+import { UserListItemToChannelComponent } from '../../shared/components/user-list-item-to-channel/user-list-item-to-channel.component';
+// import { PortalHostDirective } from "../../../../node_modules/@angular/cdk/portal-directives.d";
 
 @Component({
   selector: 'app-add-member-to-channel',
@@ -18,8 +20,7 @@ import { OverlayService } from '../../services/overlay.service';
     HeaderOverlayComponent,
     CommonModule,
     ReactiveFormsModule,
-    UserListItemComponent,
-  ],
+],
   templateUrl: './add-member-to-channel.component.html',
   styleUrl: './add-member-to-channel.component.scss',
 })
@@ -49,14 +50,32 @@ export class AddMemberToChannelComponent {
     { initialValue: [] as UserInterface[] }
   );
 
-  addMemberToArray(user: UserInterface) {
-    const exists = this.ListWithMember.some(u => u.name === user.name);
-    if(exists){
-      return;
-    }else{
-      this.ListWithMember.push(user);
-    }
+  constructor(){
+        effect(() =>{
+      this.overlayService.clearUsers();
+    })
+    effect(() => {
+      if (this.overlayService.searchReset()) {
+        this.searchControl.reset();
+        this.overlayService.clearReset();
+      }
+    })
+    effect(() =>{
+      this.ListWithMember = this.overlayService.users();
+    })
+
+    effect(() => {
+      const r = this.results();
+      if (r.length > 0) {
+        this.openAddMembersToChannel(new MouseEvent('click'));
+      }
+    });
   }
+
+  getFirstWord(name: string): string {
+    return name.split(' ')[0];  // Split by space and return the first word
+  }
+
   removeFromList(index: number) {
     this.ListWithMember.splice(index, 1);
   }
@@ -65,5 +84,26 @@ export class AddMemberToChannelComponent {
     const membersId = this.ListWithMember.map(user => user.uid);
     this.channelService.addMemberToChannel(channelId, membersId);
     this.overlayService.close();
+  }
+
+  openAddMembersToChannel(event: MouseEvent) {
+    const overlay = this.overlayService.openComponent(
+      UserListItemToChannelComponent,
+      'cdk-overlay-dark-backdrop',
+      {
+        origin: event.currentTarget as HTMLElement,
+        originPosition: {
+          originX: 'start',
+          originY: 'bottom',
+          overlayX: 'end',
+          overlayY: 'top',
+        }
+      },
+      { results: this.results,
+       }
+    );
+
+     if (!overlay) return; 
+      Object.assign(overlay.ref.instance, { overlayRef: overlay.overlayRef });
   }
 }
