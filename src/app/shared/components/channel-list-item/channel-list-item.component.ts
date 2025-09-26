bc3620816ad7662cb351c5fa73f9baeaf10db4bf
Andrei <buha2595@gmail.com>
2025-09-26 18:09:19 +0200

    feat: active channel name with diferent color

--- src/app/shared/components/channel-list-item/channel-list-item.component.ts
+++ src/app/shared/components/channel-list-item/channel-list-item.component.ts
@@ -1,11 +1,12 @@
 import { Component, EventEmitter, inject, Input, Output } from '@angular/core';
 import { ChannelInterface } from '../../models/channel.interface'; // Importing the ChannelInterface for type safety
-import { RouterLink } from '@angular/router'; // Importing Angular's Router and RouterLink for navigation
+import { ActivatedRoute, RouterLink } from '@angular/router'; // Importing Angular's Router and RouterLink for navigation
 import { CommonModule } from '@angular/common'; // Importing Angular's CommonModule for basic Angular functionality
 import { MobileService } from '../../../services/mobile.service'; // Importing the MobileService to handle mobile-specific logic
 import { ScreenService } from '../../../services/screen.service';
 import { ScreenSize } from '../../types/screen-size.type';
-import { Observable } from 'rxjs';
+import { Observable, Subject, switchMap, takeUntil } from 'rxjs';
+import { ConversationActiveRouterService } from '../../../services/conversation-active-router.service';
 
 @Component({
   selector: 'app-channel-list-item', // Component selector for this item
@@ -18,10 +19,13 @@ import { Observable } from 'rxjs';
 })
 export class ChannelListItemComponent {
   screenSize$!: Observable<ScreenSize>;
+  channelId!: string;
 
   constructor(
     public mobileService: MobileService, // Injecting MobileService for handling mobile-specific state\
-    public screenService: ScreenService
+    public screenService: ScreenService,
+    private route: ActivatedRoute,
+    private conversationActiveRouterService: ConversationActiveRouterService
   ) {
     this.screenSize$ = this.screenService.screenSize$;
   }
@@ -45,10 +49,29 @@ export class ChannelListItemComponent {
 
   @Input() isInChannelHeader = false;
 
+  private destroy$ = new Subject<void>();
+
+  active = false;
+
+  ngOnInit() {
+    this.conversationActiveRouterService
+      .getConversationId$(this.route)
+      .pipe(takeUntil(this.destroy$))
+      .subscribe((id) => {
+        this.active = this.channel.id === id;
+      });
+  }
+
+  ngOnDestroy() {
+    this.destroy$.next();
+    this.destroy$.complete();
+  }
+
   /**
    * Emits the selected channel via the channelSelected EventEmitter.
    * This is used to notify the parent component of the selected channel.
    */
+
   emitChannel() {
     this.channelSelected.emit(this.channel); // Emit the channel data to the parent component
   }
