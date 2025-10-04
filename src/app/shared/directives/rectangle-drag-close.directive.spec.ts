8146d7b2558309999a1365a88cae60881533738c
NicolausFeldtmann <n.feldtmann@yahoo.de>
2025-10-04 14:32:03 +0200

    delete: spec.ts files deleted

--- src/app/shared/directives/rectangle-drag-close.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { RectangleDragCloseDirective } from './rectangle-drag-close.directive';
-
-describe('RectangleDragCloseDirective', () => {
-  it('should create an instance', () => {
-    const directive = new RectangleDragCloseDirective();
-    expect(directive).toBeTruthy();
-  });
-});
