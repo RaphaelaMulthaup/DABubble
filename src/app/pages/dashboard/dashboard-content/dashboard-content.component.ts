5bec7554388d07d1721b59ec6dec616e5109b722
Anne Vollmann <vollmann.anne@web.de>
2025-10-09 16:17:56 +0200

    feat: added logic, style and layout to dashboard-contnent-component

--- src/app/pages/dashboard/dashboard-content/dashboard-content.component.ts
+++ src/app/pages/dashboard/dashboard-content/dashboard-content.component.ts
@@ -1,11 +1,112 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, WritableSignal } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { DashboardState } from '../../../shared/types/dashboard-state.type';
+import { BehaviorSubject, distinctUntilChanged, filter, map, Observable, shareReplay, switchMap } from 'rxjs';
+import { ScreenSize } from '../../../shared/types/screen-size.type';
+import { ScreenService } from '../../../services/screen.service';
+import { PostInterface } from '../../../shared/models/post.interface';
+import { SearchResult } from '../../../shared/types/search-result.type';
+import { toSignal } from '@angular/core/rxjs-interop';
+import { ActivatedRoute } from '@angular/router';
+import { AuthService } from '../../../services/auth.service';
+import { OverlayService } from '../../../services/overlay.service';
+import { ConversationActiveRouterService } from '../../../services/conversation-active-router.service';
 
 @Component({
   selector: 'app-dashboard-content',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './dashboard-content.component.html',
-  styleUrl: './dashboard-content.component.scss'
+  styleUrl: './dashboard-content.component.scss',
 })
-export class DashboardContentComponent {
+export class DashboardContentComponent implements OnInit {
+  dashboardState!: WritableSignal<DashboardState>;
+  screenSize$!: Observable<ScreenSize>;
+  messages$!: Observable<PostInterface[]>;
+  answers$!: Observable<PostInterface[]>;
+  results$ = new BehaviorSubject<SearchResult[]>([]);
+  resultsSignal = toSignal(this.results$.asObservable(), { initialValue: [] });
+  hasInput: boolean = false;
 
+  constructor(
+    private authService: AuthService,
+    private conversationActiveRouterService: ConversationActiveRouterService,
+    public overlayService: OverlayService,
+    private route: ActivatedRoute,
+    public screenService: ScreenService
+  ) {
+    this.dashboardState = this.screenService.dashboardState;
+    this.screenSize$ = this.screenService.screenSize$;
+  }
+
+  ngOnInit() {
+    // Initialize dashboardState with the value from the MobileService
+    // Set up the observable for fetching messages from the active conversation
+    this.messages$ = this.route.paramMap.pipe(
+      map((params) => ({
+        conversationType: params.get('conversationType'),
+        conversationId: params.get('conversationId'),
+      })),
+      // Ensure conversation type and ID are distinct before triggering the fetch
+      distinctUntilChanged(
+        (a, b) =>
+          a.conversationType === b.conversationType &&
+          a.conversationId === b.conversationId
+      ),
+      // Ensure valid conversationType and conversationId
+      filter(
+        ({ conversationType, conversationId }) =>
+          !!conversationType && !!conversationId
+      ),
+      // Fetch messages for the active conversation from the service
+      switchMap(({ conversationType, conversationId }) => {
+        this.conversationActiveRouterService['pagedMessages$'].next([]);
+        this.conversationActiveRouterService['lastVisibleMap'].delete(
+          conversationId!
+        );
+        this.conversationActiveRouterService.loadNextPage(
+          conversationType!,
+          conversationId!,
+          20
+        );
+        return this.conversationActiveRouterService.getMessages(
+          conversationType!,
+          conversationId!,
+          10
+        );
+      }),
+      // Share the last value and maintain a reference count to avoid multiple fetches
+      shareReplay({ bufferSize: 1, refCount: true })
+    );
+
+    // Set up the observable for fetching answers to a particular message in the conversation
+    this.answers$ = this.route.paramMap.pipe(
+      map((params) => ({
+        conversationType: params.get('conversationType'),
+        conversationId: params.get('conversationId'),
+        messageId: params.get('messageId'),
+      })),
+      // Ensure conversation and message IDs are distinct before triggering the fetch
+      distinctUntilChanged(
+        (a, b) =>
+          a.conversationType === b.conversationType &&
+          a.conversationId === b.conversationId &&
+          a.messageId === b.messageId
+      ),
+      // Ensure valid conversationType, conversationId, and messageId
+      filter(
+        ({ conversationType, conversationId, messageId }) =>
+          !!conversationType && !!conversationId && !!messageId
+      ),
+      // Fetch answers to a particular message
+      switchMap(({ conversationType, conversationId, messageId }) =>
+        this.conversationActiveRouterService.getAnswers(
+          conversationType!,
+          conversationId!,
+          messageId!
+        )
+      ),
+      // Share the last value and maintain a reference count to avoid multiple fetches
+      shareReplay({ bufferSize: 1, refCount: true })
+    );
+  }
 }
