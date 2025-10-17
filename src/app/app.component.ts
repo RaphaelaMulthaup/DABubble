73f17cb705028e44ea399dabda0622474ed9d20a
Andrei <buha2595@gmail.com>
2025-10-17 20:06:51 +0200

    feat:added presence-service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,8 @@ import { RouterOutlet, Router, NavigationStart } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { OverlayService } from './services/overlay.service';
 import { AuthService } from './services/auth.service';
+import { Auth, signOut } from '@angular/fire/auth';
+import { doc, Firestore, updateDoc } from '@angular/fire/firestore';
 
 @Component({
   selector: 'app-root',
@@ -14,21 +16,41 @@ export class AppComponent {
   title = 'DABubble';
 
   constructor(
+    private auth:Auth,
     private authService: AuthService,
     private router: Router,
-    private overlayService: OverlayService
+    private overlayService: OverlayService,
+    private firestore: Firestore
   ) {}
 
   ngOnInit() {
+     let isRefresh = false;
     this.router.events.subscribe((event) => {
       if (event instanceof NavigationStart) {
         this.overlayService.closeAll();
       }
     });
+    window.addEventListener("beforeunload", ()=>{
+      const user = this.authService.currentUser;
+          const userRef = doc(this.firestore, `users/${user!.uid}`);
+        const navigation = performance.getEntriesByType('navigation')[0] as PerformanceEntry;
+
+      if(navigation?.entryType === 'reload'){
+        isRefresh = true;
+      }
+
+      if(user && !isRefresh){
+        try{
+          updateDoc(userRef, { active: false });
+        }catch(err){
+          console.warn('Sing-out failed:',err);
+        }
+      }
+    })
   }
 
-  // @HostListener('window:beforeunload', ['$event'])
-  // onBeforeUnload(event: Event) {
-  //   this.authService.logout();
-  // }
+  @HostListener('window:beforeunload', ['$event'])
+  onBeforeUnload(event: Event) {
+    this.authService.logout();
+  }
 }
