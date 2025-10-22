6fef6b946de16c34bfbf18cd90bbfef62bd6393b
Raphaela Multhaup <119539243+RaphaelaMulthaup@users.noreply.github.com>
2025-10-22 10:02:17 +0200

    feat: thread for demo channel

--- src/app/services/user-demo-setup.service.ts
+++ src/app/services/user-demo-setup.service.ts
@@ -199,50 +199,50 @@ export class UserDemoSetupService {
         { createdAt: msg.createdAt }
       );
 
-      // ID der letzten Nachricht merken
-      // if (index === messages.length - 1) {
-      //   lastMessageId = messageId;
-      // } else if (index === 3) {
-      //   forthMessageId = messageId;
-      // }
-      // if (lastMessageId) {
-      //   const answers = [
-      //     {
-      //       senderId: '5lntBSrRRUM9JB5AFE14z7lTE6n1',
-      //       text: 'Montag klingt gut, wie viel Uhr?',
-      //       createdAt: new Date('2025-10-24T09:40:00'),
-      //     },
-      //     {
-      //       senderId: 'rUnD1S8sHOgwxvN55MtyuD9iwAD2',
-      //       text: 'Ich wäre ab 10 Uhr dabei!',
-      //       createdAt: new Date('2025-10-24T09:45:00'),
-      //     },
-      //     {
-      //       senderId: 'XbsVa8YOj8Nd9vztzX1kAQXrc7Z2',
-      //       text: 'Perfekt, dann planen wir 10 Uhr fest ein.',
-      //       createdAt: new Date('2025-10-24T09:50:00'),
-      //     },
-      //   ];
+      if (index === messages.length - 1) {
+        lastMessageId = messageId;
+      } else if (index === 3) {
+        forthMessageId = messageId;
+      }
+      if (lastMessageId) {
+        const answers = [
+          {
+            senderId: '5lntBSrRRUM9JB5AFE14z7lTE6n1',
+            text: 'Montag klingt gut, wie viel Uhr?',
+            createdAt: new Date('2025-10-24T09:40:00'),
+          },
+          {
+            senderId: 'rUnD1S8sHOgwxvN55MtyuD9iwAD2',
+            text: 'Ich wäre ab 10 Uhr dabei!',
+            createdAt: new Date('2025-10-24T09:45:00'),
+          },
+          {
+            senderId: 'XbsVa8YOj8Nd9vztzX1kAQXrc7Z2',
+            text: 'Perfekt, dann planen wir 10 Uhr fest ein.',
+            createdAt: new Date('2025-10-24T09:50:00'),
+          },
+        ];
 
-      //   for (const answer of answers) {
-      //     const answerId = await this.postService.createMessage(
-      //       channelDocRef.id,
-      //       answer.senderId,
-      //       answer.text,
-      //       'channel'
-      //     );
+        for (const answer of answers) {
+          const answerId = await this.postService.createAnswer(
+            channelDocRef.id,
+            lastMessageId,
+            answer.senderId,
+            answer.text,
+            'channel'
+          );
 
-      //     await updateDoc(
-      //       doc(
-      //         this.firestore,
-      //         `channels/${channelDocRef.id}/messages/${lastMessageId}/answers/${answerId}`
-      //       ),
-      //       { createdAt: answer.createdAt }
-      //     );
-      //   }
-      // } else if (forthMessageId) {
+          await updateDoc(
+            doc(
+              this.firestore,
+              `channels/${channelDocRef.id}/messages/${lastMessageId}/answers/${answerId}`
+            ),
+            { createdAt: answer.createdAt }
+          );
+        }
+      } else if (forthMessageId) {
 
-      // }
+      }
     }
   }
 
